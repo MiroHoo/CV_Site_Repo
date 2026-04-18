d6aa40ad0fbe67f81c65fcb1e0187a74682df9b6
miro hannikainen <mirohannikainen@gmail.com>
2026-04-18 23:29:11 +0300

    Added basic information without spellchecking to start working on the components

--- Frontend/CV_Website/src/components/NewStyle/Components/Header.jsx
+++ Frontend/CV_Website/src/components/NewStyle/Components/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
   return (
     <div className='w-full'>
     <img src='src\assets\NewStyle\Crows.jpg' className='select-none w-full h-[10vh] object-cover z-0 opacity-60' />
-    
     </div>
   )
 }
