1c2041272502ebd0e7d13ca3207ea0b56bf1488b
miro hannikainen <mirohannikainen@gmail.com>
2026-04-19 03:38:45 +0300

    added simple parsing of json to add elements, next is the css

--- Frontend/CV_Website/src/components/NewStyle/NewBase.jsx
+++ Frontend/CV_Website/src/components/NewStyle/NewBase.jsx
@@ -1,16 +1,67 @@
 import { useState } from 'react'
-import Header from './Components/Header'
+import Header from '../NewStyle/components/Header'
+import CvData from '../Topics.json'
 function ViewSwitcher() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='w-full h-dvh'>
-         <Header/>
-        <p>Here</p>
+      <div className=''>
+        <Header/>
+        <div>OldBase_Preview 22</div>
+        <RenderStudies/>
+        __________
+        <RenderExperience/>
+        ______________
+        <RenderProjects/>
       </div>
     </>
   )
+  function RenderStudies(){
+      const Studies = CvData.Topics[1].Schools
+      var StudyElemArrray = []; 
+      Studies.forEach(el => {
+        StudyElemArrray.push(
+          <>
+          <div>{el.Name}</div>
+          <div>{el.Date}</div>
+          <div>{el.Description}</div>
+          </>
+        )
+      })
+      
+      return StudyElemArrray
+  }
+   function RenderExperience(){
+      const Postitions = CvData.Topics[2].Positions
+      var StudyElemArrray = []; 
+      Postitions.forEach(el => {
+        StudyElemArrray.push(
+          <>
+          <div>{el.Name}</div>
+          <div>{el.Date}</div>
+          <div>{el.Description}</div>
+          </>
+        )
+      })
+
+      return StudyElemArrray
+  }
+   function RenderProjects(){
+      const Projects = CvData.Topics[3].Projects
+      var StudyElemArrray = []; 
+      Projects.forEach(el => {
+        StudyElemArrray.push(
+          <>
+          <div>{el.Name}</div>
+          <div>{el.Technologies}</div>
+          <div>{el.Description}</div>
+          </>
+        )
+      })
+
+      return StudyElemArrray
+  }
 }
 
 export default ViewSwitcher
